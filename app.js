7df649edc4f2f4fc3ba70188fb9666b1bb63c3c4
Georgia Adams <georgia.adams92@gmail.com>
2021-02-13 11:52:12 +0100

    login/signup/middleware done

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const hbs = require("hbs");
+
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 
 const indexRouter = require("./routes/index");
 const authRouter = require("./routes/auth-router");
+const mainRouter = require("./routes/main-router");
+const isLoggedIn = require("./utils/middleware");
 
 var app = express();
 
@@ -34,8 +38,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+//Cookie middleware
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    // cookie: { maxAge: 3600000 * 1 },	// 1 hour
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 60 * 60 * 24 * 7, // Time to live - 7 days (14 days - Default)
+    }),
+  })
+);
 app.use("/", indexRouter);
 app.use("/auth", authRouter);
+app.use("/user", mainRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -52,20 +70,5 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
-
-//Cookie middleware
-
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    // cookie: { maxAge: 3600000 * 1 },	// 1 hour
-    resave: true,
-    saveUninitialized: false,
-    store: new MongoStore({
-      mongooseConnection: mongoose.connection,
-      ttl: 60 * 60 * 24 * 7, // Time to live - 7 days (14 days - Default)
-    }),
-  })
-);
 
 module.exports = app;
--- routes/auth-router.js
+++ routes/auth-router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const authRouter = express.Router();
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
+const zxcvbn = require("zxcvbn");
 const saltRounds = 10;
 
 authRouter.get("/signup", (req, res, next) => {
@@ -14,6 +15,17 @@ authRouter.post("/signup", (req, res, next) => {
     res.render("auth-views/signup", {
       errorMessage: "Please enter all your details",
     });
+    return;
+  }
+  const passwordCheck = zxcvbn(password);
+  if (passwordCheck.score < 3) {
+    console.log("passwordCheck.feedback", passwordCheck.feedback);
+
+    res.render("auth-views/signup", {
+      errorMessage: passwordCheck.feedback.warning,
+      suggestions: passwordCheck.feedback.suggestions,
+    });
+
     return;
   }
   User.findOne({ email })
@@ -38,6 +50,39 @@ authRouter.post("/signup", (req, res, next) => {
         });
     })
     .catch((err) => next(err));
+});
+
+authRouter.get("/login", (req, res, next) => {
+  res.render("auth-views/login");
+});
+
+authRouter.post("/login", (req, res, next) => {
+  const { email, password } = req.body;
+  if (email === "" || password === "") {
+    res.render("auth-views/login", { errorMessage: "BYYYYEEEEE" });
+    return;
+  }
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        res.render("auth-views/login", {
+          errorMessage: "Username taken, try again",
+        });
+        return;
+      }
+      const passwordCorrect = bcrypt.compareSync(password, user.password);
+      console.log(passwordCorrect);
+      if (passwordCorrect) {
+        req.session.currentUser = user;
+        res.redirect("/");
+      } else {
+        res.render("auth-views/login", {
+          errorMessage: "Error please try again",
+        });
+      }
+    })
+    .catch((err) => console.log(err));
 });
 
 module.exports = authRouter;
